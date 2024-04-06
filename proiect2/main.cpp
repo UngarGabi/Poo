ee9613c6f2f0358da0b7ae390f6dd00a5564d1f0
UngarGabi <165913115+UngarGabi@users.noreply.github.com>
2024-04-06 18:03:02 +0300

    Update main.cpp
    Primul upgrade mai urmeaza

--- proiect2/main.cpp
+++ proiect2/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
-//Scrie nr de bani pe care vrei sa l schimbi si va parea in functie de moneda introdusa LEI/EURO/USD transformarea ei in alta moneda
+//Exchange care schimba banii cu care ai venit in alta moneda.
 class Currency {
 private:
     std::string nume;
     double cursvalutar;
+
 public:
     Currency(std::string nume, double cursvalutar) {
         this->nume = nume;
         this->cursvalutar = cursvalutar;
     }
+
     double cumpara(double lei) {
         return lei / cursvalutar;
     }
@@ -29,21 +31,26 @@ private:
     std::string nume;
     double suma;
     Currency currency;
+
 public:
     Client(std::string nume, double suma, Currency& currency) : nume(nume), suma(suma), currency(currency) {}
 
     double tranzactie(Currency& moneda) {
         return moneda.cumpara(suma);
     }
+
     double tranzactieInvers(Currency& moneda) {
         return moneda.vinde(suma);
     }
+
     double getbani() {
         return suma;
     }
+
     std::string getmoneda() {
         return currency.getnume();
     }
+
     std::string getclient() {
         return nume;
     }
@@ -56,10 +63,11 @@ public:
     void adauga(const std::string& nume) {
         numec.push_back(nume);
     }
+
     void afiseaza() {
-        std::cout << "Lista clienti:\n";
+        std::cout << "Lista clienti:" << '\n';
         for (const auto& nume : numec) {
-            std::cout << nume << "\n";
+            std::cout << nume << '\n';
         }
     }
 };
@@ -68,30 +76,54 @@ int main() {
     ListaClienti lista;
     Currency euro("EUR", 4.97);
     Currency usd("USD", 4.5);
-    double lei;
-    char moneda[10];
-    std::cout << "Nr bani pe care vrei sa-i schimbi:";
-    std::cin >> lei;
-    std::cout << "Moneda:";
-    std::cin >> moneda;
-    Client client("ANA", lei, euro);
-    Client client1("MARA",lei,usd);
-    lista.adauga(client.getclient());
-    lista.adauga(client1.getclient());
+    int ok=1;
+    double suma;
+    std::string nume;
+    char moneda[4],t[4];
+    while(ok==1) {
+        std::cout << "Nume client:";
+        std::cin >> nume;
+        std::cout << "Nr bani pe care vrei sa-i schimbi:";
+        std::cin >> suma;
+        std::cout << "Moneda(RON/EUR/USD):";
+        std::cin >> moneda;
+        Client client(nume, suma, usd);
 
-    if (strcmp("RON", moneda) == 0) {
-        double euro1 = client.tranzactie(euro);
-        std::cout << lei << " RON = " << euro1 << " " << euro.getnume() << '\n';
-        double usd1 = client.tranzactie(usd);
-        std::cout << lei << " RON = " << usd1 << " " << usd.getnume() << '\n';
-    }
-    else {
-        double lei1 = client.tranzactieInvers(euro);
-        std::cout << lei1 << " " << "RON" << '\n';
-        double lei2 = client.tranzactie(usd);
-        std::cout << lei << " " << "RON" << '\n';
-    }
+        lista.adauga(client.getclient());
+        if (strcmp("RON", moneda) == 0) {
+            std::cout << "In ce moneda schimbati?" << '\n';
+            std::cin >> t;
+            if (strcmp("EUR", t) == 0) {
+                double euro1 = client.tranzactie(euro);
+                std::cout << suma << " RON = " << euro1 << " " << euro.getnume() << '\n';
+            }
+            else
+            if (strcmp("USD", t) == 0) {
+                double usd1 = client.tranzactie(usd);
+                std::cout << suma << " RON = " << usd1 << " " << usd.getnume() << '\n';
+            }
+            else
+                std::cout << "Moneda introdusa nu este buna"<< '\n';
+        }
+        else
+        if (strcmp("EUR", moneda) == 0) {
+            double lei1 = client.tranzactieInvers(euro);
+            std::cout << suma << " " << "EUR = " << lei1 << " " << "LEI" << '\n';
+        }
+        else
+        if (strcmp("USD", moneda) == 0) {
+            double lei1 = client.tranzactieInvers(usd);
+            std::cout << suma << " " << "USD = " << lei1 << " " << "LEI" << '\n';
+        }
+        else
+            std::cout << "Moneda introdusa nu este buna" << '\n';
 
+        std::cout << "Mai sunt clienti?(Da/Nu)" << '\n';
+        std::cin >> t;
+        if(strcmp(t,"Da")!=0)
+            ok=0;
+
+    }
     lista.afiseaza();
     return 0;
 }
